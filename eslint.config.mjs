86a324185bf00b19cff43542846a94b80a0cc0b7
Stanislav Lechev [0xAF] <af@0xAF.org>
2025-05-19 04:27:41 +0300

    add /last command (and RingBuffers for background decoders)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,9 +7,15 @@ import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
+  {
+    ...tseslint.configs.recommended,
+    rules: {
+      ...(tseslint.configs.recommended.rules || {}),
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
   { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.node } },
-  tseslint.configs.recommended,
   { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
   { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
   { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
